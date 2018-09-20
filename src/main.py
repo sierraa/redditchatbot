0ffe88e4f26a88e5ed2ff3b9fae2dcfd6331ce76
sierra anderson <sielynanderson@gmail.com>
2018-09-19 22:37:49 -0700

    Read from input. Get search results as json. Fix bug in search_subreddit.

--- src/main.py
+++ src/main.py
@@ -4,11 +4,21 @@ import json
 from reddit_auth import RedditAuthentication
 from reddit_search import RedditSearch
 
+def get_subreddit_name(result):
+    return result.json()['data']['children'][0]['data']['display_name_prefixed']
+
 if __name__ == '__main__':
-    # TODO: read from input
     auth = RedditAuthentication('src/credentials.json')
-    access_token = auth.get_access_token()
-    user_agent = auth.get_user_agent()
-    search = RedditSearch(access_token, user_agent)
-    result = search.search_for_subreddit("fitness")
-    print(result.json())
+    print("Hi! I'm a reddit chatbot. Enter q at any time to quit.")
+    while True:
+        topic = input("What would you like to learn about? ")
+        if topic == "q":
+            break
+        access_token = auth.get_access_token()
+        user_agent = auth.get_user_agent()
+        search = RedditSearch(access_token, user_agent)
+        result = search.search_for_subreddit(topic)
+        subreddit_name = get_subreddit_name(result)
+        question = input("What question do you have? ")
+        result = search.search_subreddit(subreddit_name, question)
+        print(result.json())
--- src/reddit_search.py
+++ src/reddit_search.py
@@ -8,11 +8,11 @@ class RedditSearch(object):
         self.url = "https://oauth.reddit.com"
 
     def search_for_subreddit(self, keyword):
-        endpoint = self.url + "/subreddits/search?" + urllib.parse.urlencode({'q': keyword})
+        endpoint = self.url + "/subreddits/search.json?" + urllib.parse.urlencode({'q': keyword})
         return requests.get(endpoint, headers=self.headers)
 
     def search_subreddit(self, subreddit, question):
-        endpoint = self.url + "/r/" + subreddit + "/search?" + urllib.parse.urlencode({'q': question})
+        endpoint = self.url + "/" + subreddit + "/search.json?" + urllib.parse.urlencode({'q': question})
         return requests.get(endpoint, headers=self.headers)
 
     def query_comments(self, subreddit, post):
